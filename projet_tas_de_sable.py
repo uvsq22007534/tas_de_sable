97354fb7c1101c173e8f9eb343a1845991fe15fb
Nel <Nel.rivart@ens.uvsq.fr>
2022-02-08 14:11:55 +0100

    base du programme et importation tkinter

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -6,5 +6,21 @@
 # Bertille LANOIRE
 # https://github.com/uvsq-info/l1-python
 #########################################
-print("test")
-print("test 2")
+
+
+#Importation des bibliothèques
+
+import tkinter as tk
+
+#créarion de la fenêtre + gloablisation de variable taille
+global a,b
+a, b = 256, 256
+root = tk.Tk()
+root.title("tas_de_sable")
+root.geometry("100x100")
+canvas = tk.Canvas(root, width = a, height = b,bg="black")
+
+
+
+#boucle root
+root.mainloop()
