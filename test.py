694cf214a9eb3456330e2f977482e4a01e46b2aa
Nel <Nel.rivart@ens.uvsq.fr>
2022-02-09 00:24:41 +0100

    test céation aléatoire liste 2d de n nombres

--- test.py
+++ test.py
@@ -1,14 +1,53 @@
+from glob import glob
+import tkinter as tk
 import random as r
 
-global configrandom
+
+#créarion de la fenêtre + gloablisation de variable taille
+global a,b,configrandom,configbasique
+
+a, b = 5,5
+
+root = tk.Tk()
+root.title("tas de sable")
+root.geometry("600x550")
+canvas = tk.Canvas(root, width = a, height = b,bg="black")
+colors = ["blue","red","green","grey"]
+configbasique=[[0,0,0],[0,4,0],[0,0,0]]
 
 
-configrandom=[[],[],[]]
 
-configcourante=[[2,2,3],[1,6,1],[3,2,1]]
+def Random():
+    configrandom=[]
+    for i in range (a):
+        for j in range (b):
 
-for i in range (3):
-        for j in range (3):
-            configrandom[i].append(r.randint(0,10))
+            try:
 
-print(configrandom)
+                configrandom[i].append(r.randint(0,4))
+            except IndexError:
+                continue
+
+
+        print(configrandom[i])
+
+
+
+
+#Boutons
+exit = tk.Button(root, text ="Exit", command = root.destroy ,fg="red")
+exit.grid(row=0,column=0)
+
+Random1 = tk.Button(root, text ="Random", command=Random)
+Random1.grid(row=4,column=0)
+
+configbase = tk.Button(root, text ="Config base", command="")
+configbase.grid(row=5,column=0)
+
+
+#cadrillage
+grid1=canvas.grid(row=4,column=4,rowspan=1,columnspan=1)
+
+ 
+#boucle root
+root.mainloop()
