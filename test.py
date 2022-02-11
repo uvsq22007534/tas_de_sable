2b59380b23a1eb2dca49659dd5d46a0255820f92
Nel <Nel.rivart@ens.uvsq.fr>
2022-02-11 19:16:17 +0100

    test

--- test.py
+++ test.py
@@ -15,21 +15,27 @@
 
 import tkinter as tk
 import random as r
-import time 
+import time
+
+from pyparsing import CaselessKeyword 
 
 
 ########################### PARAMETRE FENETRE + VARIABLE GLOBAL #######################################"
 
 global a,b,configbasique
-a, b = 100,100
+a, b = 750,750
 
 root = tk.Tk()
 root.title("tas de sable")
-root.geometry("600x550")
+root.geometry("1920x1080")
 canvas = tk.Canvas(root, width = a, height = b,bg="grey")
+txt1=tk.Text(root,height=5,width=35)
 colors = ["blue","red","green","grey"]
 configbasique=[[0,0,0],[0,4,0],[0,0,0]]
-
+nbcase=35
+x0=1
+y0=1
+taillecase=22
 
 ########################### FONCTIONS #######################################"
 
@@ -49,7 +55,7 @@ def Random():
 
             except IndexError:
                 continue
-        
+          
 
     print("Configuration aléatoire crée : ")
     print("")
@@ -57,6 +63,12 @@ def Random():
     print(printclear(configrandom))
     configcourante=configrandom
     coloration()  
+
+
+def grille():
+    for i in range(nbcase+1):
+        canvas.create_line(x0+taillecase*i, y0,x0+taillecase*i,y0 + nbcase*taillecase)
+        canvas.create_line(x0, y0+taillecase*i,x0+nbcase*taillecase ,y0+taillecase*i)
 
 #Fonction qui définit la configuration basique comme configuration courante (et l'afffiche)
 def basique():
@@ -128,7 +140,7 @@ def coloration():
             else:
                 draw_pixel(i,j,"yellow")
 
-#je sais plus ca sert a quoi j'etait fatiguer 
+#je sais plus ca sert a quoi j'etais fatiguer 
 def launch():
     while True:
         for i in range (len(configcourante)):
@@ -154,7 +166,20 @@ def stabilisation():
     print("Fin !")   
     print(i-1," Itérations on été nécessaire pour venir a bout de ce tas de sable !") 
 
+#Fonction qui permet d'obtenir la position en temps réel de la souris sur le canvas
+def posmouse(event):
+    posx,posy=event.x,event.y
+    txt1.delete('1.0',"end")
+    txt1.insert("end","clic detecte en x="+str(event.x) + " et y = " + str(event.y))
+    return (posx,posy)
 
+
+
+def clic(event):
+    return
+
+
+  
 
 ########################### BOUTONS #######################################"
 
@@ -162,16 +187,16 @@ exit = tk.Button(root, text ="Exit", command = root.destroy ,fg="red")
 exit.grid(row=0,column=0)
 
 Random1 = tk.Button(root, text ="Random", command=Random)
-Random1.grid(row=4,column=0)
+Random1.grid(row=1,column=0)
 
 distribution = tk.Button(root, text ="Distribution ", command=distrib)
-distribution.grid(row=5,column=0)
+distribution.grid(row=2,column=0)
 
 colo = tk.Button(root, text ="Coloration", command=coloration)
-colo.grid(row=6,column=0)
+colo.grid(row=3,column=0)
 
 base  = tk.Button(root, text ="Basique", command=basique)
-base.grid(row=7,column=0)
+base.grid(row=4,column=0)
 
 
 lancement=tk.Button(root, text ="Launch", command=launch)
@@ -183,12 +208,17 @@ stabi.grid(row=0,column=2)
 courante=tk.Button(root, text ="Courante", command=cfgcourante)
 courante.grid(row=0,column=3)
 
+affigrille=tk.Button(root, text ="grille", command=grille)
+affigrille.grid(row=0,column=4)
 
 
 
 ########################### grid #######################################"
-grid1=canvas.grid(row=10,column=4,rowspan=10,columnspan=10)
+grid1=canvas.grid(row=1,column=1)
+grid2=txt1.grid(row=1, column=2)
 
+canvas.bind("<Button-1>",posmouse)
+canvas.bind("<Motion>",posmouse)
 
 
 ########################### MAINLOOP du programme #######################################"
